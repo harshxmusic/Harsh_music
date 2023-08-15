164cf14a4fcb74e26becd97f27830c2636589192
harshxmusic <142278878+harshxmusic@users.noreply.github.com>
2023-08-15 09:58:36 +0530

    Create owner.py

---
+++ AnonX/plugins/owner.py
@@ -0,0 +1,107 @@
+from pyrogram import Client, filters
+import requests
+import random
+import os
+import re
+import asyncio
+import time
+from AnonX import app
+
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+
+@app.on_message(
+    filters.command("owner")
+    & filters.group
+    & ~filters.edited & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/8998ddca2f42a4fac4efd.jpg",
+        caption=f"""🍁𝐂ʅιƈ𝐊🥰𝐁ҽʅσ𝐖💝𝐁υƚƚσ𝐍✨𝐓σ🙊𝐃ɱ❤️𝐎ɯɳҽɾ𝐑🍁""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🌹 𝐎ɯɳҽɾ𝐑 🌹", url=f"https://t.me/HARSH_XD_FIGHTER")
+                ]
+            ]
+        ),
+    )
+
+@app.on_message(
+    filters.command("owner")
+    & filters.private
+    & ~filters.edited & filters.private & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/8998ddca2f42a4fac4efd.jpg",
+        caption=f"""🍁𝐂ʅιƈ𝐊🥰𝐁ҽʅσ𝐖💝𝐁υƚƚσ𝐍✨𝐓σ🙊𝐃ɱ❤️𝐎ɯɳҽ𝐑🍁""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🌹 𝐎ɯɳҽɾ𝐑 🌹", url=f"https://t.me/HARSH_XD_FIGHTER")
+                ]
+            ]
+        ),
+    )
+
+
+
+
+
+
+@app.on_message(
+    filters.command("support")
+    & filters.group
+    & ~filters.edited & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/987c3d0db44f6dd58fb2e.jpg",
+        caption=f"""🍁𝐂ʅυƈ𝐊🥰𝐁ҽʅσ𝐖💝𝐁υƚƚσ𝐍✨𝐓σ🙊𝐆ҽ𝐓🌱𝐒υρρσɾ𝐓🍁""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🌱𝐒υρρσɾ𝐓🌱", url=f"https://t.me/anonymusc_supporter")
+                ]
+            ]
+        ),
+    )
+
+@app.on_message(
+    filters.command("support")
+    & filters.group
+    & ~filters.edited & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/987c3d0db44f6dd58fb2e.jpg",
+        caption=f"""🍁𝐂ʅιƈ𝐊🥰𝐁ҽʅσ𝐖💝𝐁υƚƚσ𝐍✨𝐓σ🙊𝐆ҽ𝐓🌱𝐒υρρσ𝐓🍁""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🌱𝐒υρρσɾ𝐓🌱", url=f"https://t.me/anonymusc_supporter")
+                ]
+            ]
+        ),
+    )
+
+@app.on_message(
+    filters.command("support")
+    & filters.private
+    & ~filters.edited & filters.private & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/987c3d0db44f6dd58fb2e.jpg",
+        caption=f"""🍁𝐂ʅιƈ𝐊🥰𝐁ҽʅσ𝐖💝𝐁υƚƚσ𝐍✨𝐓σ🙊𝐆ҽ𝐓🌱𝐒υρρσɾ𝐓🍁""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🌱𝐒υρρσɾ𝐓🌱", url=f"https://t.me/anonymusc_supporter")
+                ]
+            ]
+        ),
+    )
